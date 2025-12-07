3e15fbb23c046ae75e53eb605d933732a43250bd
JBF <jermesonferreira747@gmail.com>
2025-12-07 04:10:12 -0300

    atulização tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"], // garante que todas as classes no HTML sejam processadas
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
